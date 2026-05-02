b020bc1b4e4a90136d4682020b1f6abbe0a35a33
Elijah Bevers <elijah.bevers@oit.edu>
2026-05-01 20:37:57 -0700

    working to characterize the transform

--- LinearOperators/discrete_sums.py
+++ LinearOperators/discrete_sums.py
@@ -2,6 +2,15 @@ import scipy
 import numpy as np
 from pyperclip import copy, paste
 
+def sample_summ_op_diag(L, N):
+    x = np.linspace(-L, L, N)
+    dx = x[1] - x[0]
+    I = np.eye(N)
+    D = (I - np.concatenate((I[:, 1:], np.zeros((N, 1))), 1)) / dx
+    S = np.linalg.inv(I - scipy.linalg.expm(-D))
+    diag = np.flip(S - I, axis=1).diagonal()
+    return diag / np.max(diag)
+
 # f(n) = sum(g(k), 0 <= k <= n)
 # f(n) - f(n - 1) = g(n); f(0) = 0
 # (1 - e^-D)f = g; f(0) = 0
@@ -10,8 +19,9 @@ def solve_sum_approx(f, x0, x1, N):
     dx = x[1] - x[0]
     I = np.eye(N)
     D = (I - np.concatenate((I[:, 1:], np.zeros((N, 1))), 1)) / dx
+    S = np.linalg.inv(I - scipy.linalg.expm(-D))
     f = f(x)
-    F = np.dot(np.linalg.inv(I - scipy.linalg.expm(-D)), f)
+    F = np.dot(S, f)
     return locals()
 
 def copy_to_desmos(data):
